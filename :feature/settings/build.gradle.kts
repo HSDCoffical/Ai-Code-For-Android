21a2a147471726bfeb5bcdba8dec5f2765bcc94b
凉数中 <2791389901@qq.com>
2026-08-17 17:18:53 +0800

    创建 :feature/settings/build.gradle.kts

---
+++ :feature/settings/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins { id("com.android.library"); id("org.jetbrains.kotlin.android"); id("kotlin-kapt"); id("com.google.dagger.hilt.android") }
+android { namespace = "com.example.feature.settings" }
+dependencies {
+    implementation(project(":core:data"))
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+}
