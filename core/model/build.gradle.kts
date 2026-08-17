9b2fe947eacde8dfe3538d8160cf63797ce952c2
凉数中 <2791389901@qq.com>
2026-08-17 20:18:41 +0800

    更新 build.gradle.kts

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    id("java-library")  // 改为 java-library
-    id("org.jetbrains.kotlin.jvm")  // 添加 jvm 插件
+    id 'java-library'
+    id 'kotlin'
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
 dependencies {
-    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation 'com.squareup.moshi:moshi:1.15.0'
 }
