9b31950ea498e22a63a4c9449be732b8346b2abb
凉数中 <2791389901@qq.com>
2026-08-17 20:20:02 +0800

    更新 build.gradle.kts

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    id 'com.android.library'
+    id 'kotlin-android'
+    id 'kotlin-kapt'
+    id 'dagger.hilt.android.plugin'
 }
 
 android {
-    namespace = "com.example.feature.settings"
-    compileSdk = 34
+    namespace 'com.example.feature.settings'
+    compileSdk 34
 }
 
 dependencies {
-    implementation(project(":core:data"))
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("com.google.dagger:hilt-android:2.48")
-    add("kapt", "com.google.dagger:hilt-compiler:2.48")
+    implementation project(':core:data')
+    implementation 'androidx.compose.material3:material3:1.2.0'
+    implementation 'androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0'
+    implementation 'com.google.dagger:hilt-android:2.48'
+    kapt 'com.google.dagger:hilt-compiler:2.48'
 }
