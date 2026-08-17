1e1cfa343b8f49c0dc7327a907a37c0498d03882
凉数中 <2791389901@qq.com>
2026-08-17 17:16:51 +0800

    创建 :feature:chat/build.gradle.kts

---
+++ :feature:chat/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+android { namespace = "com.example.feature.chat" }
+dependencies {
+    implementation(project(":core:data"))
+    implementation(project(":core:model"))
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+}
