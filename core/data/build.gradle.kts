1d2fe307c73b1c53bcfef5c261992f2b8a238774
凉数中 <2791389901@qq.com>
2026-08-17 20:19:28 +0800

    更新 build.gradle.kts

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    id 'com.android.library'
+    id 'kotlin-android'
+    id 'kotlin-kapt'
+    id 'dagger.hilt.android.plugin'
 }
 
 android {
-    namespace = "com.example.core.data"
-    compileSdk = 34
+    namespace 'com.example.core.data'
+    compileSdk 34
 }
 
 dependencies {
-    implementation(project(":core:model"))
-    implementation(project(":core:network"))
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("com.google.dagger:hilt-android:2.48")
-    add("kapt", "com.google.dagger:hilt-compiler:2.48")  // 关键修改
+    implementation project(':core:model')
+    implementation project(':core:network')
+    implementation 'androidx.datastore:datastore-preferences:1.0.0'
+    implementation 'com.google.dagger:hilt-android:2.48'
+    kapt 'com.google.dagger:hilt-compiler:2.48'
 }
