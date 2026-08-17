e743fb5d74037415db2c6977412b27cc5792bc5c
凉数中 <2791389901@qq.com>
2026-08-17 20:13:41 +0800

    更新 build.gradle.kts

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -4,14 +4,16 @@ plugins {
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
+
 android {
     namespace = "com.example.feature.settings"
     compileSdk = 34
 }
+
 dependencies {
     implementation(project(":core:data"))
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    add("kapt", "com.google.dagger:hilt-compiler:2.48")
 }
