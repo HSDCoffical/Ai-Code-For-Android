ea88929ef9c6359c0362e8cf2946a290761ba70d
凉数中 <2791389901@qq.com>
2026-08-18 22:10:32 +0800

    创建 app/src/main/java/com/example/myaitest/MainActivity.java

---
+++ app/src/main/java/com/example/myaitest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myaitest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello from Java!\n应用启动成功！");
+        tv.setTextSize(24);
+        tv.setGravity(android.view.Gravity.CENTER);
+        setContentView(tv);
+    }
+}
